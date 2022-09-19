f2b9b0a643863610759943fb38375661a6b5d688
Taxiarchoula Karanika <100430160+Taxiarchoula@users.noreply.github.com>
2022-09-19 12:27:10 +0300

    Delete assets directory

--- assets/js/hamburger_menu.js
+++
@@ -1,7 +0,0 @@
-const menu_toggle = document.querySelector('.menu-toggle');
-		const sidebar = document.querySelector('.sidebar');
-
-menu_toggle.addEventListener('click', () => {
-			menu_toggle.classList.toggle('active');
-			sidebar.classList.toggle('active');
-});
