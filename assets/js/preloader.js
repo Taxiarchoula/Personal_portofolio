95e3d5e0c4c79926318139529a046241d8c99e29
Taxiarchoula Karanika <100430160+Taxiarchoula@users.noreply.github.com>
2022-09-20 15:31:35 +0300

    Add files via upload

---
+++ assets/js/preloader.js
@@ -0,0 +1,21 @@
+//preloader
+
+$(document).ready(function() {
+//Preloader
+preloaderFadeOutTime = 5000;
+function hidePreloader() {
+var preloader = $('.spinner-wrapper');
+preloader.fadeOut(preloaderFadeOutTime);
+}
+hidePreloader();
+});
+$(document).ready(function() {
+  //Preloader
+  preloaderFadeOutTime = 5000;
+  function hidePreloade1() {
+  var preloader = $('.spinner');
+  preloader.fadeOut(preloaderFadeOutTime);
+  }
+  hidePreloader1();
+  });
+  
